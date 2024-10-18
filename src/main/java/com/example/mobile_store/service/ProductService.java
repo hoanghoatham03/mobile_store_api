2c32b48b7306de07b19c8a3b9151146db9591640
hoanghoatham03 <hoanghoatham03122003.com>
2024-10-18 21:10:54 +0700

    add pagination product

--- src/main/java/com/example/mobile_store/service/ProductService.java
+++ src/main/java/com/example/mobile_store/service/ProductService.java
@@ -5,6 +5,8 @@ import com.example.mobile_store.dto.ProductDTO;
 import com.example.mobile_store.entity.Product;
 import com.example.mobile_store.mapper.ProductMapper;
 import com.example.mobile_store.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,20 +47,23 @@ public class ProductService {
     }
 
 
-    //get all products
-    public List<ProductDTO> getAllProducts() {
-        List<Product> products = productRepository.findAll();
+    //get all products paginated
+    public List<ProductDTO> getAllProducts(Pageable pageable) {
+        Page<Product> products = (Page<Product>) productRepository.findAll(pageable);
 
-        List<ProductDTO> productDTOs = products.stream()
+        List<Product> productList = products.getContent();
+
+        List<ProductDTO> productDTOs = productList.stream()
                 .map(productMapper::toDTO)
                 .collect(Collectors.toList());
-
 
         String prefix = "http://localhost:8080/resources/images/product/";
         productDTOs.forEach(dto -> dto.setImage(prefix + dto.getImage()));
 
         return productDTOs;
+
     }
+
 
     //view product by id
     public ProductDTO getProductById(Integer id) {
