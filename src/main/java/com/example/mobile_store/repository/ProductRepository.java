1cf00b54649beced71175d65f0be3c0c965e7c86
hoanghoatham03 <hoanghoatham03122003.com>
2024-10-20 19:10:43 +0700

    feature filter product

--- src/main/java/com/example/mobile_store/repository/ProductRepository.java
+++ src/main/java/com/example/mobile_store/repository/ProductRepository.java
@@ -3,17 +3,19 @@ package com.example.mobile_store.repository;
 import com.example.mobile_store.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
     Page<Product> findAll(Pageable pageable);
 
-    List<Product> findByProductNameContainingIgnoreCase(String productName);
+    Page<Product> findAll(Specification<Product> spec, Pageable pageable);
 
     boolean existsByProductName(String productName);
 
